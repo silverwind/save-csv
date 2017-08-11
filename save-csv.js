f09a726728768fa616a61a84dc26f58082099e83
silverwind <me@silverwind.io>
2017-08-11 18:58:08 +0200

    re-enable auto separator based on Number.prototype.toLocaleString

--- save-csv.js
+++ save-csv.js
@@ -16,17 +16,18 @@
 
     opts = opts || {};
     opts.filename = opts.filename || "export.csv";
-    opts.sep = opts.sep || ",";
+    opts.sep = opts.sep || "auto";
     opts.eol = opts.eol || "\r\n";
     opts.bom = typeof opts.bom === "boolean" ? opts.bom : true;
     opts.quote = opts.quote || '"';
     opts.mime = opts.mime || "text/csv;charset=utf-8";
 
-    if (opts.sep === "auto" && "toLocaleString" in []) {
-      var sep = ["", ""].toLocaleString();
-      opts.sep = sep ? sep : ",";
-    } else {
-      opts.sep = opts.sep || ",";
+    // This is not ideal, but given that Array.prototype.toLocaleString is
+    // rather unreliable, it's a good compromise.
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=1388777
+    // https://bugs.chromium.org/p/chromium/issues/detail?id=753841
+    if (opts.sep === "auto" && "toLocaleString" in Number.prototype) {
+      opts.sep = (1.2).toLocaleString().substring(1, 2) === "," ? ";" : ",";
     }
 
     var quoteRe = new RegExp(opts.quote, "g");
